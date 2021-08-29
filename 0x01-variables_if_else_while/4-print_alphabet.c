e23c8fe4d5723d7e503f1adcb0f79ab2fcddd831
Albert Disi <disialbert1998@gmail.com>
2021-08-29 15:18:23 +0300

    Nesting if statement in for loop

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		if (l != 'e' && l != 'q')
+			putchar(l);
+	putchar('\n');
+	return (0);
+}
