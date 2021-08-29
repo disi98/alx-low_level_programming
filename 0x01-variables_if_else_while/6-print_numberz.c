95da58b1d6ac22b4b9513141e719fc53fa61d60b
Albert Disi <disialbert1998@gmail.com>
2021-08-29 15:53:59 +0300

    printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of the code
+ *
+ * Return: Always returns 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
