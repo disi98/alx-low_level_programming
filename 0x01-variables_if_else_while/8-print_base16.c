cc7a471db92b82b1a87577bc89fe794793d3a1e9
Albert Disi <disialbert1998@gmail.com>
2021-08-29 16:11:19 +0300

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	char a;
+
+	for (n = '0'; n < '9'; n++)
+		putchar(n);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
