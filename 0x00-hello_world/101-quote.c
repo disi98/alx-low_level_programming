1559f1086de86881af2b2f69f8362737354995ac
Albert Disi <disialbert1998@gmail.com>
2021-08-30 16:10:16 +0300

    Simplified C while loop for printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - starting point
+ *
+ * Return: 1 success
+ */
+int main(void)
+{
+	char *h="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	while (*h)
+		putchar(*h++);
+	putchar('\n');
+	return (1);
+}
