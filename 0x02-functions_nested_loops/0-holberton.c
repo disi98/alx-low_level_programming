1aa9cec7eb3b2f7fb5f0c00e6ecc74afa6825bf5
Albert Disi <disialbert1998@gmail.com>
2021-08-31 16:36:10 +0300

    C functions and header #thingsaregettingraelyhard

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ * main - starting point
+ *
+ * Return: always (0)
+ */
 int main(void)
 {
-	char c[10] = "Holberton";
+	char c[10] = "_putchar";
 	int i;
 
 	for (i =0; i < 10; i++)
