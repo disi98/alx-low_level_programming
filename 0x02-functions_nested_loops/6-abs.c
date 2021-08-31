a9a5a46db7dfbbd61ce0c2dfde68091a30054320
Albert Disi <disialbert1998@gmail.com>
2021-08-31 19:06:03 +0300

    absolut integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - refers to the main header
  * @r: number of type int
- * Description: This function prints the last digit of a number
+ * Description: prints the last digit of a number
  * Return: last digit of number r
  */
 int _abs(int r)
