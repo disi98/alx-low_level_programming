037d527b8c4df2c30f4f00a91b69e8c85e808a17
Albert Disi <disialbert1998@gmail.com>
2021-09-03 14:29:54 +0300

    Multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplier function
+ * @a: input parameter
+ * @b: input parameter
+ *
+ * Return: always 0 success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
