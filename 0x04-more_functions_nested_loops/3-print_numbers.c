e8970316c88594d4283c9d87446ba7c302b77202
Albert Disi <disialbert1998@gmail.com>
2021-09-03 14:37:51 +0300

    Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - refers to header main
+ * _putchar - prints the required results
+ *
+ * Return: always 0 success
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for(n = 0; n <= 9; n++)
+		_putchar(n);
+	_putchar('\n');
+	return (0);
+}
