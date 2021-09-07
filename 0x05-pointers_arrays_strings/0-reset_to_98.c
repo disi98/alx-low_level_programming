fc505e63adfbff1226c609ce0ab035464353e347
Albert Disi <disialbert1998@gmail.com>
2021-09-07 19:56:00 +0300

    Reset var n using the reset function on pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - functions that set the input to 98
+ * @n: input parameter of type int
+ * Return: always 0 success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
