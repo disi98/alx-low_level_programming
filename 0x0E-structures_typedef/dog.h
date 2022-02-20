ad979ce608e58c3c98959bf10804f138f208804f
Albert Disi <disialbert1998@gmail.com>
2022-02-20 13:53:56 +0300

    structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines the dog structure
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
+typedef sruct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+
+#endif
